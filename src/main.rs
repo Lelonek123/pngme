3adb9f4dbe2e485c7199eee5dd23c103640ff936
lelonek <lelonek1234@gmail.com>
2024-03-23 18:07:01 +0100

    [chapter_1] Added Error type enum

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod chunk;
 mod chunk_type;
 mod commands;
 mod png;
+mod error;
 
+use crate::error::Error;
 
-pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
